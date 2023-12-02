62b4b6c4fa056c14846503e90bf568d9c0614146
Aron Jinga <aronjinga@gmail.com>
2023-12-02 15:54:50 +0100

    Catkin build on python 3

---
+++ ros_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/aron/Desktop/UHH/HRI_project/ros_ws/src
+# Build directory: /home/aron/Desktop/UHH/HRI_project/ros_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
